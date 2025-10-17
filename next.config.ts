d932773822d075cf7ac18c50d39c0e923031ad3a
NAK110 <vathanak110@gmail.com>
2025-10-17 11:30:30 +0700

    fix bugs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920]
+    }
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
